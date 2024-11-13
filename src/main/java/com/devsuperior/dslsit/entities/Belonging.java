7d7f85a26a85337c4258b4e7f105bce06729a83c
Guilherme Rossan <guilherme.rossan@sinqia.com.br>
2024-11-13 12:22:51 -0300

    Implementando modelo de dominio

---
+++ src/main/java/com/devsuperior/dslsit/entities/Belonging.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dslsit.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_beloging")
+public class Belonging {
+    @EmbeddedId // Está falando que essa propriedade vai representar os dois ids que estão no beloginPk
+    private BelongingPK id = new BelongingPK();
+    private Integer position;
+
+    public Belonging(){}
+
+    public Belonging(Game game, GameList list, Integer position) {
+        id.setGame(game);
+        id.setList(list);
+        this.position = position;
+    }
+
+    public BelongingPK getId() {
+        return id;
+    }
+
+    public void setId(BelongingPK id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
